ad5ffcfa7565e3e3a3e82e6db64938690dcc3772
agnieszkachmielnicka <agnieszka@chmielnicka.com.pl>
2020-05-15 21:38:22 +0200

    add gitignore and cleaning

--- restapienv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\eagnchm\documents\workspace\rest_api_project\restapienv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
